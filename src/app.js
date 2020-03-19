e250a0f31c4a719cace1e27af2471edfe526a2df
Rafael Monte <rsmonte@hotmail.com>
2020-03-19 16:45:04 -0300

    Adicionado Controler, e metodo put para cadastrar produtos

--- src/app.js
+++ src/app.js
@@ -1,12 +1,44 @@
 'use strict'
 const express = require('express');
-const bodyParser = require('body-parser')
+const mongoose = require("mongoose");
+const bodyparser = require('body-parser');
+
+
 const app = express();
+//Conectar Banco
+var url =
+    "mongodb+srv://rafael:rafael@lanchoneteapp-xocup.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log("conectado!!");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+
+
+
+//controller
+const Produto = require('./models/produto');
 //Rotas
-const rotaIndex = require('../src/routes/rotateste')
-const rotaPratos = require('../src/routes/rotapratos')
+const rotaIndex = require('../src/routes/rotateste');
+const rotaProdutos = require('../src/routes/rotaprodutos');
+
+app.use(bodyparser.json({
+  limit: '5mb'
+}));
+app.use(bodyparser.urlencoded({
+  extended: false
+}));
+
 app.use('/',rotaIndex);
-app.use('/Pratos',rotaPratos);
-app.use('/Pratos/:id',rotaPratos);
+app.use('/Produtos',rotaProdutos);
 
 module.exports = app;
